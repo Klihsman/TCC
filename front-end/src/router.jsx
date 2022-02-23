c99bcfc44ff9de8f37bd7901e9007f6ef78f1f17
Klihsman Freitas <klihsman@gmail.com>
2022-02-23 08:33:26 -0300

    add login page

--- front-end/src/router.jsx
+++ front-end/src/router.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes}  from 'react-router-dom';
 
+import Home from './Pages/Home';
+import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 
 const Router = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/signup' element={<SignUp />} />
+                <Route path='/' exact element={<Home />}></Route>
+
+                <Route path='/login' element={<Login />} />
+                
+                <Route path='/signup' element={<SignUp />} /> 
             </Routes>
         </BrowserRouter>
     )

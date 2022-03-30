e38d1a637b1532d88090215e83253ba060ada62c
Klihsman Freitas <klihsman@gmail.com>
2022-03-30 08:47:47 -0300

    add partial header and home page

---
+++ front-end/src/Pages/Search/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { NormalHeader } from '../../Components/Utils/Header';
+
+const Search = ()=> {
+    return(
+        <div className='height-100vh width-max flex-container'>
+            <NormalHeader />
+        </div>
+    )
+}
+
+export default Search;
--- front-end/src/router.jsx
+++ front-end/src/router.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes}  from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
+import Search from './Pages/Search';
 
 const Router = () =>{
     return(
@@ -14,6 +15,7 @@ const Router = () =>{
                 <Route path='/login' element={<Login />} />
                 
                 <Route path='/signup' element={<SignUp />} /> 
+                <Route path='/search' element={<Search />} />
             </Routes>
         </BrowserRouter>
     )
